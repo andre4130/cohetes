9f2a8647e91c9b5142ce86264d166c92c3f1a5e2
Andre de Mendonça Maia <60257960+andre4130@users.noreply.github.com>
2020-04-28 09:53:25 +0200

    Delete rocket.ts

--- controller/rocket.ts
+++
@@ -1,9 +0,0 @@
-class Rocket{
-  id:string;
-  numberOfPropellers:number;
-  
-  constructor(id:string,numberOfPropellers:number){
-    this.id=id;
-    this.numberOfPropellers=numberOfPropellers;
-  }
-};
