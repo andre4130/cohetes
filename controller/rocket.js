ece8296fdb58405e7056005d94a9eb07dd5bdcc9
Andre de Mendonça Maia <60257960+andre4130@users.noreply.github.com>
2020-04-28 09:53:18 +0200

    Delete rocket.js

--- controller/rocket.js
+++
@@ -1,9 +0,0 @@
-var Rocket = /** @class */ (function () {
-    function Rocket(id, numberOfPropellers, propellerPower) {
-        this.id = id;
-        this.numberOfPropellers = numberOfPropellers;
-        this.propellerPower = propellerPower;
-    }
-    return Rocket;
-}());
-;
